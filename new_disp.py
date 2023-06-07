14b22e94e6a47c3457fecba37c654d01ae306c8e
Tanay Sinha <tanay.sinha@mail.utoronto.ca>
2023-06-07 23:44:38 +0530

    Add code to display various colored widgets -- starting point for cubies

--- new_disp.py
+++ new_disp.py
@@ -1,26 +1,36 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget
+from PySide6.QtGui import QPalette, QColor
+from PySide6.QtWidgets import QHBoxLayout, QVBoxLayout, QGridLayout
 
+class Color(QWidget):
+
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(color))
+        self.setPalette(palette)
 
 class MainWindow(QMainWindow):
+
     def __init__(self):
-        super().__init__()
+        super(MainWindow, self).__init__()
 
         self.setWindowTitle("My App")
 
-        self.label = QLabel()
+        layout = QGridLayout()
 
-        self.input = QLineEdit()
-        self.input.textChanged.connect(self.label.setText)
+        layout.addWidget(Color('red'), 0, 1)
+        layout.addWidget(Color('green'), 1, 0)
+        layout.addWidget(Color('blue'), 1, 1)
+        layout.addWidget(Color('purple'), 2, 1)
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.input)
-        layout.addWidget(self.label)
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
 
-        container = QWidget()
-        container.setLayout(layout)
-
-        self.setCentralWidget(container)
 
 
 app = QApplication(sys.argv)
@@ -29,3 +39,13 @@ window = MainWindow()
 window.show()
 
 app.exec_()
+
+
+"""
+TODO:
+
+1. Add function to make a square 'widget' --- cubie
+2. Text input for scrambles
+3. Solver button --- pop up into soln alg / display soln alg?
+4. Option to click on face and put that face's color
+"""
