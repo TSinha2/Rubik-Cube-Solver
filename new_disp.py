1dc3e71f3554cfb5dc3b8560b2ee20e1198f6abe
Tanay Sinha <tanay.sinha@mail.utoronto.ca>
2023-06-08 22:39:54 +0530

    Modify code to draw face

--- new_disp.py
+++ new_disp.py
@@ -4,7 +4,6 @@ from PySide6.QtGui import QPalette, QColor
 from PySide6.QtWidgets import QHBoxLayout, QVBoxLayout, QGridLayout
 
 class Color(QWidget):
-
     def __init__(self, color):
         super(Color, self).__init__()
         self.setAutoFillBackground(True)
@@ -13,8 +12,10 @@ class Color(QWidget):
         palette.setColor(QPalette.Window, QColor(color))
         self.setPalette(palette)
 
-class MainWindow(QMainWindow):
+        self.setFixedSize(50, 50)  # Set the fixed width and height of the widget
 
+
+class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
@@ -22,15 +23,13 @@ class MainWindow(QMainWindow):
 
         layout = QGridLayout()
 
-        layout.addWidget(Color('red'), 0, 1)
-        layout.addWidget(Color('green'), 1, 0)
-        layout.addWidget(Color('blue'), 1, 1)
-        layout.addWidget(Color('purple'), 2, 1)
+        for row in range(3):
+            for col in range(3):
+                layout.addWidget(Color('red'), row, col)
 
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
-
 
 
 app = QApplication(sys.argv)
@@ -39,13 +38,3 @@ window = MainWindow()
 window.show()
 
 app.exec_()
-
-
-"""
-TODO:
-
-1. Add function to make a square 'widget' --- cubie
-2. Text input for scrambles
-3. Solver button --- pop up into soln alg / display soln alg?
-4. Option to click on face and put that face's color
-"""
