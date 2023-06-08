f4dcc40c6f3c9782ee928092635f6338075661af
Tanay Sinha <tanay.sinha@mail.utoronto.ca>
2023-06-08 23:09:47 +0530

    Modify code to take a face

--- new_disp.py
+++ new_disp.py
@@ -5,7 +5,7 @@ from PySide6.QtWidgets import QVBoxLayout, QGridLayout
 from PySide6.QtCore import Qt
 
 class CubeSide(QWidget):
-    def __init__(self, color):
+    def __init__(self, colors):
         super(CubeSide, self).__init__()
 
         layout = QGridLayout()
@@ -15,7 +15,7 @@ class CubeSide(QWidget):
                 color_widget = QWidget()
                 color_widget.setAutoFillBackground(True)
                 palette = color_widget.palette()
-                palette.setColor(QPalette.Window, QColor(color))
+                palette.setColor(QPalette.Window, QColor(colors[row][col]))
                 color_widget.setPalette(palette)
                 layout.addWidget(color_widget, row, col)
 
@@ -30,23 +30,41 @@ class CubeNet(QWidget):
 
         layout = QGridLayout()
 
-        # Yellow side
-        layout.addWidget(CubeSide('yellow'), 0, 1)
+        # Yellow side with a grid of green squares
+        colors_green = [['green', 'green', 'green'],
+                        ['green', 'green', 'green'],
+                        ['green', 'green', 'green']]
+        layout.addWidget(CubeSide(colors_green), 0, 1)
 
-        # White side
-        layout.addWidget(CubeSide('white'), 2, 1)
+        # White side with a grid of red squares
+        colors_red = [['red', 'red', 'red'],
+                      ['red', 'red', 'red'],
+                      ['red', 'red', 'red']]
+        layout.addWidget(CubeSide(colors_red), 2, 1)
 
-        # Red side
-        layout.addWidget(CubeSide('red'), 1, 1)
+        # Red side with a grid of blue squares
+        colors_blue = [['blue', 'blue', 'blue'],
+                       ['blue', 'blue', 'blue'],
+                       ['blue', 'blue', 'blue']]
+        layout.addWidget(CubeSide(colors_blue), 1, 1)
 
-        # Blue side
-        layout.addWidget(CubeSide('blue'), 1, 0)
+        # Blue side with a grid of yellow squares
+        colors_yellow = [['yellow', 'yellow', 'yellow'],
+                         ['yellow', 'yellow', 'yellow'],
+                         ['yellow', 'yellow', 'yellow']]
+        layout.addWidget(CubeSide(colors_yellow), 1, 0)
 
-        # Orange side
-        layout.addWidget(CubeSide('orange'), 1, 2)
+        # Orange side with a grid of purple squares
+        colors_purple = [['purple', 'purple', 'purple'],
+                         ['purple', 'purple', 'purple'],
+                         ['purple', 'purple', 'purple']]
+        layout.addWidget(CubeSide(colors_purple), 1, 2)
 
-        # Green side
-        layout.addWidget(CubeSide('green'), 1, 3)
+        # Green side with a grid of orange squares
+        colors_orange = [['orange', 'orange', 'orange'],
+                         ['orange', 'orange', 'orange'],
+                         ['orange', 'orange', 'orange']]
+        layout.addWidget(CubeSide(colors_orange), 1, 3)
 
         self.setLayout(layout)
 
@@ -56,4 +74,4 @@ app = QApplication(sys.argv)
 window = CubeNet()
 window.show()
 
-app.exec_()
+app.exec()
