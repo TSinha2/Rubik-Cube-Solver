4bba0bd593e265281618dc0165fe7530e1a39989
Tanay Sinha <tanay.sinha@mail.utoronto.ca>
2023-06-08 23:07:43 +0530

    Display all sides of cube

--- new_disp.py
+++ new_disp.py
@@ -1,40 +1,59 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget
-from PySide6.QtGui import QPalette, QColor
-from PySide6.QtWidgets import QHBoxLayout, QVBoxLayout, QGridLayout
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel
+from PySide6.QtGui import QPalette, QColor, QFont
+from PySide6.QtWidgets import QVBoxLayout, QGridLayout
+from PySide6.QtCore import Qt
 
-class Color(QWidget):
+class CubeSide(QWidget):
     def __init__(self, color):
-        super(Color, self).__init__()
-        self.setAutoFillBackground(True)
-
-        palette = self.palette()
-        palette.setColor(QPalette.Window, QColor(color))
-        self.setPalette(palette)
-
-        self.setFixedSize(50, 50)  # Set the fixed width and height of the widget
-
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-
-        self.setWindowTitle("My App")
+        super(CubeSide, self).__init__()
 
         layout = QGridLayout()
 
         for row in range(3):
             for col in range(3):
-                layout.addWidget(Color('red'), row, col)
+                color_widget = QWidget()
+                color_widget.setAutoFillBackground(True)
+                palette = color_widget.palette()
+                palette.setColor(QPalette.Window, QColor(color))
+                color_widget.setPalette(palette)
+                layout.addWidget(color_widget, row, col)
 
-        widget = QWidget()
-        widget.setLayout(layout)
-        self.setCentralWidget(widget)
+        self.setLayout(layout)
+
+
+class CubeNet(QWidget):
+    def __init__(self):
+        super(CubeNet, self).__init__()
+
+        self.setWindowTitle("Cube Net")
+
+        layout = QGridLayout()
+
+        # Yellow side
+        layout.addWidget(CubeSide('yellow'), 0, 1)
+
+        # White side
+        layout.addWidget(CubeSide('white'), 2, 1)
+
+        # Red side
+        layout.addWidget(CubeSide('red'), 1, 1)
+
+        # Blue side
+        layout.addWidget(CubeSide('blue'), 1, 0)
+
+        # Orange side
+        layout.addWidget(CubeSide('orange'), 1, 2)
+
+        # Green side
+        layout.addWidget(CubeSide('green'), 1, 3)
+
+        self.setLayout(layout)
 
 
 app = QApplication(sys.argv)
 
-window = MainWindow()
+window = CubeNet()
 window.show()
 
 app.exec_()
